6c79dde15f64e19a2563b949832da8c374a74d1a
Lane Votapka <lvotapka100@gmail.com>
2021-07-13 12:42:12 -0600

    Set tests to run using Reference platform with Openmm.

--- seekr2/tests/test_elber_sim_openmm.py
+++ seekr2/tests/test_elber_sim_openmm.py
@@ -9,6 +9,7 @@ from seekr2.modules import elber_sim_openmm
 
 def test_elber_sim_openmm_amber(tmp_path, tryp_ben_elber_model):
     myanchor = tryp_ben_elber_model.anchors[1]
+    tryp_ben_elber_model.openmm_settings.cuda_platform_settings = None
     output_file = os.path.join(tmp_path, "output.txt")
     my_sim_openmm = elber_sim_openmm.create_sim_openmm(
         tryp_ben_elber_model, myanchor, output_file)
@@ -18,6 +19,7 @@ def test_elber_sim_openmm_amber(tmp_path, tryp_ben_elber_model):
 def test_elber_sim_openmm_forcefield(tmp_path, tryp_ben_elber_model):
     # TODO: un-skip this test if forcefield inputs ever become widely-used
     myanchor = tryp_ben_elber_model.anchors[1]
+    tryp_ben_elber_model.openmm_settings.cuda_platform_settings = None
     output_file = os.path.join(tmp_path, "output.txt")
     my_sim_openmm = elber_sim_openmm.create_sim_openmm(
         tryp_ben_elber_model, myanchor, output_file)
