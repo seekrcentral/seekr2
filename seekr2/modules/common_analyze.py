Fixed bug caused by numerical instability when analyzing systems with long timescales.

seekr2/modules/common_analyze.py
@@ -31,7 +31,7 @@ class MissingStatisticsError(Exception):
     """Catch a very specific type of error in analysis stage."""
     pass
 
-def solve_rate_matrix(Q_hat, max_iter=100, ETOL=1e-10):
+def solve_rate_matrix(Q_hat, max_iter=100, ETOL=1e-15):
     """
     Use an infinite series combined with a divide-and-conquer method
     to solve the Q-matrix since the numpy/scipy solver tends to have 
@@ -39,15 +39,35 @@ def solve_rate_matrix(Q_hat, max_iter=100, ETOL=1e-10):
     """
     
     K_hat = Q_to_K(Q_hat)
-    sum_of_series = np.identity(K_hat.shape[0], dtype=np.longdouble)
+    K_pow_n = np.linalg.matrix_power(K_hat, 0)
+    sum_of_series_tmp = np.identity(K_hat.shape[0], dtype=np.longdouble)
+    section = np.identity(K_hat.shape[0], dtype=np.longdouble)
+    
+    section_list = []
     for i in range(max_iter):
         n = 2**i
         K_pow_n = np.linalg.matrix_power(K_hat, n)
-        section = K_pow_n @ sum_of_series
-        sum_of_series += section
+        assert np.isfinite(K_pow_n).all()
+        section = np.dot(K_pow_n, sum_of_series_tmp)
+        assert np.isfinite(sum_of_series_tmp).all()
+        assert np.isfinite(section).all()
+        sum_of_series_tmp += section
+        section_list.append(section)
         error = np.linalg.norm(section)
+        if not np.isfinite(error):
+            break
+        if i == max_iter-1:
+            print("Warning: solve_rate_matrix reached max iterations. "\
+                  "Results may be incorrect.")
         if error < ETOL:
             break
+        
+    sum_of_series = np.zeros(K_hat.shape, dtype=np.longdouble)
+    section_list.reverse()
+    for section in section_list:
+        sum_of_series += section
+    sum_of_series += np.identity(K_hat.shape[0], dtype=np.longdouble)
+    
     T = np.zeros(Q_hat.shape, dtype=np.longdouble)
     t_vector = np.zeros((Q_hat.shape[0], 1), dtype=np.longdouble)
     for i in range(Q_hat.shape[0]):
@@ -67,6 +87,15 @@ def Q_to_K(Q):
                 K[i,j] = 0.0
             else:
                 K[i,j] = -Q[i,j] / Q[i,i]
+    
+    """            
+    for i in range(K.shape[0]):
+        for j in range(K.shape[0]):
+            if i == j:
+                K[i,j] = 0.0
+            else:
+                K[i,j] = -K[i,j] / np.sum(K[i,:])
+    """     
     return K
 
 def quadriture(err1, err2):
@@ -550,7 +579,7 @@ class Data_sample():
         for i in range(self.model.num_milestones):
             row_sum = np.sum(self.Q[i])
             if row_sum == 0:
-                new_row_sum = 0.0
+                new_row_sum = np.longdouble(0.0)
                 for j in range(self.model.num_milestones):
                     self.Q[i,j] = self.Q[j,i]
                     new_row_sum += self.Q[j,i]
@@ -694,11 +723,7 @@ class Data_sample():
         for end_milestone_dest in end_milestones:
             if end_milestone_dest in bulk_milestones:
                 continue
-            Q_hat = minor2d(self.Q[:], end_milestone_dest, end_milestone_dest)
-            #I = np.zeros((len(Q_hat)), dtype = float)    
-            #I[:] = 1.0
-            #end_state_times = la.solve(Q_hat, -I)
-            end_state_times = solve_rate_matrix(Q_hat)
+            
             for end_milestone_src in end_milestones:
                 if end_milestone_dest == end_milestone_src:
                     # don't get the MFPT from a milestone to itself
@@ -706,6 +731,14 @@ class Data_sample():
                 if end_milestone_src in bulk_milestones:
                     # a bulk milestone will never be a source
                     continue
+                # TODO: move above this loop? Can save time, but Elber
+                #  milestoning calls the bulk state an end state, which is
+                #  a problem
+                Q_hat_endstate = minor2d(self.Q[:], end_milestone_dest, end_milestone_dest)
+                #I = np.zeros((len(Q_hat_endstate)), dtype = float)    
+                #I[:] = 1.0
+                #end_state_times = la.solve(Q_hat_endstate, -I)
+                end_state_times = solve_rate_matrix(Q_hat_endstate)
                 mfpt = end_state_times[end_milestone_src]
                 MFPTs[(end_milestone_src, end_milestone_dest)] = mfpt
         

seekr2/modules/mmvt_analyze.py
@@ -5,9 +5,28 @@ Routines for the analysis stage of an MMVT calculation
 from collections import defaultdict
 
 import numpy as np
+import scipy as sp
 import scipy.linalg as la
+import scipy.sparse.linalg as sp
+
+FLUX_MATRIX_K_EXPONENT = 1000
 
 import seekr2.modules.common_analyze as common_analyze
+
+def flux_matrix_to_K(M):
+    """Given a flux matrix M, compute probability transition matrix K."""
+    
+    K = np.zeros((M.shape[0]-1, M.shape[0]-1), dtype=np.longdouble)
+    for i in range(M.shape[0]-1):
+        for j in range(M.shape[0]-1):
+            if i == j:
+                K[i,j] = 0.0
+            else:
+                K[i,j] = -M[i,j] / M[i,i]
+                
+            assert K[i,j] >= 0.0, "Negative values for K matrix not allowed."
+    
+    return K
 
 def openmm_read_output_file_list(output_file_list, min_time=None, max_time=None, 
                                  existing_lines=[], skip_restart_check=False):
@@ -554,6 +573,7 @@ class MMVT_anchor_statistics():
                             "'namd', or 'browndye'.")
         for key in self.N_alpha_beta:
             self.k_alpha_beta[key] = self.N_alpha_beta[key] / self.T_alpha_total
+            
         assert self.T_alpha_total >= 0.0
         #assert len(self.k_alpha_beta) > 0, \
         #    "Missing statistics for anchor %d" % anchor.index
@@ -728,9 +748,9 @@ class MMVT_data_sample(common_analyze.Data_sample):
                             "No statistics present in Data Sample.")
         
         flux_matrix_dimension = self.model.num_anchors
-        self.pi_alpha = np.zeros(flux_matrix_dimension)
-        flux_matrix = np.zeros((flux_matrix_dimension, flux_matrix_dimension))
-        column_sum = np.zeros(flux_matrix_dimension)
+        self.pi_alpha = np.zeros(flux_matrix_dimension, dtype=np.longdouble)
+        flux_matrix = np.zeros((flux_matrix_dimension, flux_matrix_dimension), dtype=np.longdouble)
+        column_sum = np.zeros(flux_matrix_dimension, dtype=np.longdouble)
         bulk_index = None
         for alpha in range(flux_matrix_dimension):
             anchor1 = self.model.anchors[alpha] # source anchor
@@ -753,7 +773,7 @@ class MMVT_data_sample(common_analyze.Data_sample):
                     if id_alias is None:
                         flux_matrix[beta, alpha] = 0.0
                     else:
-                        flux_matrix[beta, alpha] = 1.0
+                        flux_matrix[beta, alpha] = 1e15
                 else:
                     if alpha == beta:
                         pass 
@@ -780,10 +800,22 @@ class MMVT_data_sample(common_analyze.Data_sample):
             
         assert bulk_index is not None, "A bulk state has not been defined, "\
             "but is required."
-        prob_equil = np.zeros((flux_matrix_dimension,1))
+        prob_equil = np.zeros((flux_matrix_dimension,1), dtype=np.longdouble)
         prob_equil[bulk_index] = 1.0
-        
         self.pi_alpha = abs(la.solve(flux_matrix.T, prob_equil))
+        
+        # refine pi_alpha
+        self.pi_alpha = self.pi_alpha.astype(dtype=np.longdouble)
+        pi_alpha_slice = np.zeros((flux_matrix_dimension-1, 1), dtype=np.longdouble)
+        for i in range(flux_matrix_dimension-1):
+            pi_alpha_slice[i,0] = -self.pi_alpha[i,0] * flux_matrix[i,i]
+        K = flux_matrix_to_K(flux_matrix)
+        K_inf = np.linalg.matrix_power(K, FLUX_MATRIX_K_EXPONENT)
+        stationary_dist = K_inf.T @ pi_alpha_slice
+        for i in range(flux_matrix_dimension-1):
+            self.pi_alpha[i,0] = -stationary_dist[i,0] / flux_matrix[i,i]
+        self.pi_alpha[-1,0] = 0.0
+        self.pi_alpha = self.pi_alpha / np.sum(self.pi_alpha)
         return
     
     def fill_out_data_quantities(self):
@@ -797,24 +829,24 @@ class MMVT_data_sample(common_analyze.Data_sample):
             raise Exception("Unable to call fill_out_data_quantities(): "\
                             "No statistics present in Data Sample.")
             
-        self.N_ij = defaultdict(float)
-        self.R_i = defaultdict(float)
-        self.T = 0.0
-        sum_pi_alpha_over_T_alpha = 0.0
+        self.N_ij = defaultdict(np.longdouble)
+        self.R_i = defaultdict(np.longdouble)
+        self.T = np.longdouble(0.0)
+        sum_pi_alpha_over_T_alpha = np.longdouble(0.0)
         for alpha, anchor in enumerate(self.model.anchors):
             if anchor.bulkstate:
                 continue
-            this_anchor_pi_alpha = float(self.pi_alpha[alpha])
+            this_anchor_pi_alpha = np.longdouble(self.pi_alpha[alpha])
             sum_pi_alpha_over_T_alpha += this_anchor_pi_alpha \
-                / self.T_alpha[alpha]
+                / np.longdouble(self.T_alpha[alpha])
         
-        self.T = 1.0 / sum_pi_alpha_over_T_alpha
+        self.T = sum_pi_alpha_over_T_alpha ** -1
         assert self.T >= 0.0, "self.T should be positive: {}".format(self.T)
         for alpha, anchor in enumerate(self.model.anchors):
             if anchor.bulkstate:
                 continue
-            this_anchor_pi_alpha = float(self.pi_alpha[alpha])
-            T_alpha = self.T_alpha[alpha]
+            this_anchor_pi_alpha = np.longdouble(self.pi_alpha[alpha])
+            T_alpha = np.longdouble(self.T_alpha[alpha])
             assert T_alpha >= 0.0, \
                 "T_alpha should be positive: {}".format(T_alpha)
             time_fraction = self.T / T_alpha
@@ -823,28 +855,28 @@ class MMVT_data_sample(common_analyze.Data_sample):
             for key in N_i_j_alpha:
                 assert time_fraction > 0.0, \
                     "time_fraction should be positive: {}".format(time_fraction)
-                assert this_anchor_pi_alpha > 0.0, \
+                assert this_anchor_pi_alpha >= 0.0, \
                     "this_anchor_pi_alpha should be positive: {}".format(
                         this_anchor_pi_alpha)
                 assert N_i_j_alpha[key] > 0.0, \
                     "N_i_j_alpha[key] should be positive: {}".format(
                         N_i_j_alpha[key])
                 self.N_ij[key] += time_fraction * \
-                    this_anchor_pi_alpha * N_i_j_alpha[key]
+                    this_anchor_pi_alpha * np.float128(N_i_j_alpha[key])
             
             R_i_alpha = self.R_i_alpha[alpha]
             if len(R_i_alpha) > 0:
                 for key in R_i_alpha:
                     assert time_fraction > 0.0, \
                         "time_fraction should be positive: {}".format(time_fraction)
-                    assert this_anchor_pi_alpha > 0.0, \
+                    assert this_anchor_pi_alpha >= 0.0, \
                         "this_anchor_pi_alpha should be positive: {}".format(
                             this_anchor_pi_alpha)
                     assert R_i_alpha[key] > 0.0, \
                         "R_i_alpha[key] should be positive: {}".format(
                             R_i_alpha[key])
                     self.R_i[key] += time_fraction * this_anchor_pi_alpha * \
-                        R_i_alpha[key]
+                        np.longdouble(R_i_alpha[key])
                         
             else:
                 raise Exception("R_i_alpha should always be set.")

seekr2/tests/test_analyze.py
@@ -53,7 +53,8 @@ def make_smoluchowski_standard_for_k_off(mymodel, potential_energy_function):
 
 def make_smoluchowski_mmvt_analysis(mymodel, potential_energy_function, 
                                     k_on_src=None, transition_probs=None,
-                                    diffusion=1.0, beta=1.0):
+                                    diffusion=1.0, beta=1.0, 
+                                    pre_equilibrium_approx=False):
     calc = smol.make_smoluchowski_calculation_from_model(
         mymodel, potential_energy_function, beta=beta, diffusion=diffusion)
     my_analysis = analyze.Analysis(mymodel)
@@ -110,12 +111,13 @@ def make_smoluchowski_mmvt_analysis(mymodel, potential_energy_function,
     
     my_analysis.main_data_sample.compute_rate_matrix()
     my_analysis.main_data_sample.calculate_thermodynamics()
-    my_analysis.main_data_sample.calculate_kinetics()
+    my_analysis.main_data_sample.calculate_kinetics(
+        pre_equilibrium_approx=pre_equilibrium_approx)
     mmvt_time = my_analysis.main_data_sample.MFPTs[(0,"bulk")]
     k_on = None
     if k_on_src is not None:
         k_on = my_analysis.main_data_sample.k_ons[0]
-    return mmvt_time, k_on
+    return mmvt_time, k_on, my_analysis
 
 def make_smoluchowski_elber_analysis(mymodel, potential_energy_function,
                                      k_on_src=None, transition_probs=None,
@@ -146,7 +148,7 @@ def make_smoluchowski_elber_analysis(mymodel, potential_energy_function,
         # transition info
         my_analysis.main_data_sample.bd_transition_probabilities[0] = transition_probs
         my_analysis.main_data_sample.bd_transition_counts[0] = transition_probs
-        
+    
     my_analysis.main_data_sample.compute_rate_matrix()
     #self.main_data_sample.Q = common_analyze.minor2d(
     #    self.main_data_sample.Q, bulkstate, bulkstate)
@@ -177,26 +179,26 @@ def make_smoluchowski_elber_analysis(mymodel, potential_energy_function,
     k_on = None
     if k_on_src is not None:
         k_on = my_analysis.main_data_sample.k_ons[0]
-    return elber_time, k_on
+    return elber_time, k_on, my_analysis
     
 def test_smoluchowski_k_off_from_mmvt_statistics(smoluchowski_mmvt_model):
     smoluchowski_mmvt_model.k_on_info = None
     potential_energy_function = smol.FlatPotentialEnergyFunction()
-    mmvt_time, dummy = make_smoluchowski_mmvt_analysis(
+    mmvt_time, dummy, dummy2 = make_smoluchowski_mmvt_analysis(
         smoluchowski_mmvt_model, potential_energy_function)
     standard_time = make_smoluchowski_standard_for_k_off(
         smoluchowski_mmvt_model, potential_energy_function)
     assert np.isclose(mmvt_time, standard_time, rtol=1e-3)
     
     potential_energy_function = smol.LinearPotentialEnergyFunction()
-    mmvt_time, dummy = make_smoluchowski_mmvt_analysis(
+    mmvt_time, dummy, dummy2 = make_smoluchowski_mmvt_analysis(
         smoluchowski_mmvt_model, potential_energy_function)
     standard_time = make_smoluchowski_standard_for_k_off(
         smoluchowski_mmvt_model, potential_energy_function)
     assert np.isclose(mmvt_time, standard_time, rtol=1e-3)
     
     potential_energy_function = smol.QuadraticPotentialEnergyFunction(a=0.2)
-    mmvt_time, dummy = make_smoluchowski_mmvt_analysis(
+    mmvt_time, dummy, dummy2 = make_smoluchowski_mmvt_analysis(
         smoluchowski_mmvt_model, potential_energy_function)
     standard_time = make_smoluchowski_standard_for_k_off(
         smoluchowski_mmvt_model, potential_energy_function)
@@ -240,7 +242,7 @@ def test_smoluchowski_k_on_from_mmvt_statistics(smoluchowski_mmvt_model):
     k_on_src, transition_probs = make_bd_statistics_flat(
         smoluchowski_mmvt_model, D)
     potential_energy_function = smol.FlatPotentialEnergyFunction()
-    mmvt_time, k_on = make_smoluchowski_mmvt_analysis(
+    mmvt_time, k_on, dummy = make_smoluchowski_mmvt_analysis(
         smoluchowski_mmvt_model, potential_energy_function, k_on_src, transition_probs)
     bound_state_radius = smoluchowski_mmvt_model.anchors[0].milestones[0].variables["radius"]
     standard_k_on = 4.0 * np.pi * bound_state_radius * D
@@ -252,7 +254,7 @@ def test_smoluchowski_k_on_from_mmvt_statistics(smoluchowski_mmvt_model):
     potential_energy_function = smol.CoulombicPotentialEnergyFunction(q1q2=q1q2, a=bound_state_radius)
     k_on_src, transition_probs = make_bd_statistics_coulomb(
         smoluchowski_mmvt_model, D, q1q2, beta)
-    mmvt_time, k_on = make_smoluchowski_mmvt_analysis(
+    mmvt_time, k_on, dummy = make_smoluchowski_mmvt_analysis(
         smoluchowski_mmvt_model, potential_energy_function, k_on_src, transition_probs)
     
     standard_k_on = -q1q2 * D / (1.0 - np.exp(q1q2/(4.0*np.pi*bound_state_radius)))
@@ -262,21 +264,21 @@ def test_smoluchowski_k_on_from_mmvt_statistics(smoluchowski_mmvt_model):
 def test_smoluchowski_k_off_from_elber_statistics(smoluchowski_elber_model):
     smoluchowski_elber_model.k_on_info = None
     potential_energy_function = smol.FlatPotentialEnergyFunction()
-    elber_time, dummy = make_smoluchowski_elber_analysis(
+    elber_time, dummy, dummy2 = make_smoluchowski_elber_analysis(
         smoluchowski_elber_model, potential_energy_function)
     standard_time = make_smoluchowski_standard_for_k_off(
         smoluchowski_elber_model, potential_energy_function)
     assert np.isclose(elber_time, standard_time, rtol=1e-3)
     
     potential_energy_function = smol.LinearPotentialEnergyFunction()
-    elber_time, dummy = make_smoluchowski_elber_analysis(
+    elber_time, dummy, dummy2 = make_smoluchowski_elber_analysis(
         smoluchowski_elber_model, potential_energy_function)
     standard_time = make_smoluchowski_standard_for_k_off(
         smoluchowski_elber_model, potential_energy_function)
     assert np.isclose(elber_time, standard_time, rtol=1e-3)
     
     potential_energy_function = smol.QuadraticPotentialEnergyFunction(a=0.2)
-    elber_time, dummy = make_smoluchowski_elber_analysis(
+    elber_time, dummy, dummy2 = make_smoluchowski_elber_analysis(
         smoluchowski_elber_model, potential_energy_function)
     standard_time = make_smoluchowski_standard_for_k_off(
         smoluchowski_elber_model, potential_energy_function)
@@ -288,7 +290,7 @@ def test_smoluchowski_k_on_from_elber_statistics(smoluchowski_elber_model):
     k_on_src, transition_probs = make_bd_statistics_flat(
         smoluchowski_elber_model, D)
     potential_energy_function = smol.FlatPotentialEnergyFunction()
-    elber_time, k_on = make_smoluchowski_elber_analysis(
+    elber_time, k_on, dummy = make_smoluchowski_elber_analysis(
         smoluchowski_elber_model, potential_energy_function, k_on_src, transition_probs)
     bound_state_radius = smoluchowski_elber_model.anchors[0].milestones[0].variables["radius"]
     standard_k_on = 4.0 * np.pi * bound_state_radius * D
@@ -300,7 +302,7 @@ def test_smoluchowski_k_on_from_elber_statistics(smoluchowski_elber_model):
     potential_energy_function = smol.CoulombicPotentialEnergyFunction(q1q2=q1q2, a=bound_state_radius)
     k_on_src, transition_probs = make_bd_statistics_coulomb(
         smoluchowski_elber_model, D, q1q2, beta)
-    elber_time, k_on = make_smoluchowski_elber_analysis(
+    elber_time, k_on, dummy = make_smoluchowski_elber_analysis(
         smoluchowski_elber_model, potential_energy_function, k_on_src, transition_probs)
     
     standard_k_on = -q1q2 * D / (1.0 - np.exp(q1q2/(4.0*np.pi*bound_state_radius)))

seekr2/toy/smoluchowski.py
@@ -139,7 +139,7 @@ class SmoluchowskiSphericalRegion():
         denominator = 4.0*np.pi*outer_integral
         self.outward_A1 = 1.0/denominator
         if np.isclose(denominator, 0.0):
-            print("denominator is nearly zero:", denominator)
+            print("smoluchowski.py: denominator is nearly zero:", denominator)
             
         # Compute numerator
         for i, r in enumerate(r_s_forward):
@@ -187,7 +187,7 @@ class SmoluchowskiSphericalRegion():
         denominator = 4.0*np.pi*simps(denominator_vals, dx=self.h)
         self.inward_A1 = 1.0/denominator
         if np.isclose(denominator, 0.0):
-            print("denominator is nearly zero:", denominator)
+            print("smoluchowski.py denominator is nearly zero:", denominator)
         
         for i, r in enumerate(r_s_forward):
             w = self.potential_energy_function.evaluate_energy(r)
@@ -401,7 +401,7 @@ class Simulation():
 class SmoluchowskiCalculation1d():
     def __init__(self, potential_energy_function, milestones, 
                  absorbing_boundary, reflecting_boundary=0.0, diffusion=1.0,
-                 beta=1.0):
+                 beta=1.0, n=101):
         self.potential_energy_function = potential_energy_function
         self.milestones = milestones
         self.absorbing_boundary = absorbing_boundary
@@ -409,30 +409,34 @@ class SmoluchowskiCalculation1d():
         self.regions = []
         self.diffusion = diffusion
         self.beta = beta
+        self.n = n
         self.partition_function = 0.0
         self.make_regions()
         self.make_elber_statistics()
         self.make_state_transition_rates()
+        
         return
     
     def make_regions(self):
         first_region = SmoluchowskiSphericalRegion(
             self.reflecting_boundary, self.milestones[0], 
-            self.potential_energy_function, self.diffusion, self.beta)
+            self.potential_energy_function, self.diffusion, self.beta, self.n)
+        
         self.regions.append(first_region)
         self.partition_function += first_region.partition_function
         
         for i, milestone in enumerate(self.milestones[:-1]):
             region = SmoluchowskiSphericalRegion(
                 self.milestones[i], self.milestones[i+1], 
-                self.potential_energy_function, self.diffusion, self.beta)
+                self.potential_energy_function, self.diffusion, self.beta, 
+                self.n)
             
             self.regions.append(region)
             self.partition_function += region.partition_function
             
         last_region = SmoluchowskiSphericalRegion(
             self.milestones[-1], self.absorbing_boundary,
-            self.potential_energy_function, self.diffusion, self.beta)
+            self.potential_energy_function, self.diffusion, self.beta, self.n)
         self.regions.append(last_region)
         self.partition_function += last_region.partition_function
         
@@ -452,12 +456,13 @@ class SmoluchowskiCalculation1d():
                 down_rate = N_MAGNITUDE*self.regions[i].N_alpha_down_up
             else:
                 down_rate = 10000.0
-            up_rate = (b*down_rate - prev_rate)/a
+            up_rate_for_prev = (b)*down_rate - prev_rate
+            up_rate = (b/a)*down_rate - prev_rate/a
             #self.downward_rates.append(down_rate)
             #self.upward_rates.append(up_rate)
             self.regions[i].N_alpha_up = up_rate
             self.regions[i+1].N_alpha_down = down_rate
-            prev_rate = -a*up_rate + b*down_rate
+            prev_rate = -up_rate_for_prev + b*down_rate
             
         #print("self.upward_rates:", self.upward_rates)
         #print("self.downward_rates:", self.downward_rates)