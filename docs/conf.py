5ae66e82ddaeaa5821f91d5138b16a30e3bb0c4b
Lane Votapka <lvotapka100@gmail.com>
2023-10-27 14:27:53 -0600

    Trying to debug RTD 9

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
     'sphinx.ext.extlinks',
+    'sphinx_rtd_theme',
 ]
 
 autosummary_generate = True
@@ -74,7 +75,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
